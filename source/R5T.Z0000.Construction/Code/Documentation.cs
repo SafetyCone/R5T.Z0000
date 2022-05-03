e897bd4adba449e13ad6930d61f37094af4f7b4b
davidcoats <david@getsafetycone.com>
2022-05-03 12:12:59 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Z0000.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0000.Construction
+{
+    /// <summary>
+    /// Construction project for Common values of .NET C# built-in types, like characters, strings, integers, etc..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
